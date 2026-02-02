dc8e1fa32a57b759486065358bf917bfaba8ebf7
Daniel Miller <daniel@matv.io>
2026-02-02 16:20:23 -0300

    Updated antigravity to nixpkgs unstable

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,12 +7,18 @@
 let
   overlays = {
     modifications = final: prev: {
+      antigravity =
+        (import inputs.nixpkgs-unstable {
+          system = final.system;
+          config.allowUnfree = true;
+        }).antigravity;
     };
-    additions = final: _prev: import ../packages {
-      inherit lib;
-      pkgs = final;
-    };
+    additions =
+      final: _prev:
+      import ../packages {
+        inherit lib;
+        pkgs = final;
+      };
   };
 in
 overlays
-
