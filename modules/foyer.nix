244287217da27277f0207dd7801c44414b5d30b4
Daniel Miller <daniel@matv.io>
2026-04-22 20:36:02 -0700

    Updated foyer to properly report temps on raven

--- modules/foyer.nix
+++ modules/foyer.nix
@@ -151,7 +151,7 @@ in
       wantedBy = [ "multi-user.target" ];
 
       path = lib.optional config.virtualisation.docker.enable config.virtualisation.docker.package
-        ++ lib.optional (cfg.temperatureCommand != "") pkgs.openssh;
+        ++ lib.optionals (cfg.temperatureCommand != "") [ pkgs.openssh pkgs.iproute2 pkgs.coreutils pkgs.gawk ];
 
       serviceConfig = {
         Type = "simple";
